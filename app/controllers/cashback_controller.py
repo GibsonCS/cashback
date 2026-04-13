e87ffbc5e01179b709e1b0e712799262eb817b6e
GibsonCS <gibson.cruzds@gmail.com>
2026-04-13 19:56:58 -0300

    feat: add handle get all queiries controller

--- app/controllers/cashback_controller.py
+++ app/controllers/cashback_controller.py
@@ -1,4 +1,4 @@
-from app.services.cashback_service import calculate_cashback
+from app.services.cashback_service import calculate_cashback, get_queries_cashback
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Request, Depends
 from app.schemas.CashbackRequest import CashbackRequest
@@ -9,3 +9,11 @@ router = APIRouter()
 @router.post("/cashback")
 async def handleCashback(cashback_request: CashbackRequest, request: Request, db: Session = Depends(get_db)):
         return await calculate_cashback(cashback_request, request.client.host, db)
+
+@router.get("/cashback")
+async def handleGetQueriesCashback(request: Request, db: Session = Depends(get_db)):
+        
+        if(request.client.host == ""):
+                raise RuntimeWarning("ip not found")
+        
+        return await get_queries_cashback(request.client.host, db)
